376ce12f3822de3199ac275ebea4ffb072afb0ce
Laura Mendieta <laumendieta06@gmail.com>
2024-06-18 16:11:40 -0500

    [CREATE] EJERCICIO 3

---
+++ javascript/Actividad6/scripts/ejercicio3.js
@@ -0,0 +1,19 @@
+console.log("Estoy conectado");
+
+/*
+    for
+    forEach
+    while
+    Do While
+*/
+
+
+// EJERCICIO 3
+/*
+3.) Mostrar números:
+Escribe un programa que use un bucle for para imprimir los números del 1 al 10 en la consola.
+*/
+
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
